61f2f0cd4e0564582af2b3989451d718f8aca8d8
Pallavi <pallavi@Pallavis-MacBook-Pro.local>
2024-04-13 19:37:00 +0530

    modify the code acc to my req

--- swiggy.py
+++ swiggy.py
@@ -12,41 +12,22 @@ HEADERS = {
     'Content-Type': 'application/json'
     }
 
-GET_ORDERS_URL = 'https://www.swiggy.com/dapi/order/all?order_id='
+SWIGGY_URL = 'https://www.swiggy.com/dapi/order/all'
 
 def getOrders(cookies):
     print("Retrieving...")
     spent = 0
-    s = requests.Session()
-    last_order_id = ''
     num_of_orders = 0
-    while 1:
-        # 10 orders retrieved in each api call
-        URL = ''
-        if last_order_id!='':
-            URL = GET_ORDERS_URL+str(last_order_id).strip()
-        else:
-            URL = GET_ORDERS_URL
-
-        r = s.get(URL, headers=HEADERS, cookies=cookies)
-        resp = json.loads(r.text)
-        if resp['statusCode']==1:
-            print("[red][-] Status Code is 1, exiting[/red]")
-            break
-
-        if len(resp['data']['orders'])==0:
-            print("Reached end of orders")
-            break
-        for order in resp['data']['orders']:
-            order_id = order['order_id']
-            order_total = order['order_total']
-            # print(order_total)
-            num_of_orders+=1
-            spent+=order_total
+    req = requests.get(SWIGGY_URL, headers=HEADERS, cookies=cookies)
+    final_data = json.loads(req.text)
+    for order in final_data['data']['orders']:
+        order_id = order['order_id']
+        print(order_id)
+        order_total = order['order_total']
+        print(order_total)
+        num_of_orders+=1
+        spent+=order_total
         
-
-        last_order_id = resp['data']['orders'][-1]['order_id']
-    
     average_spent = spent//num_of_orders
     print()
     print(f"[green]Total money spent on swiggy.com : [bold]INR {spent:,}[/bold][/green]")
@@ -80,32 +61,9 @@ def cookiesToDict():
     return cookies 
 
 
-def checkLogin(cookies):
-    # First check if logged in
-    print("[green][+][/green] Checking if session is valid")
-    r = requests.get(GET_ORDERS_URL, headers=HEADERS, cookies=cookies)
-    resp = None
-    try:
-        resp = json.loads(r.text)
-    except Exception as e:
-        print("[red][-] Unexpected Response received[/red]")
-        return False
-
-    if 'statusCode' not in resp or 'data' not in resp:
-        print("[red][-] Unexpected Response received[/red]")
-        return False
-    if resp['statusCode'] == 1:
-        print("[red][-] Not logged in, check cookies and try again[/red]")
-        return False
-    
-    return True
-
 
 if __name__ == "__main__":
     print("Started Script..:vampire:")
     cookies = cookiesToDict()
-    if cookies is None:
-        sys.exit()
-    if checkLogin(cookies):
-        getOrders(cookies)
+    getOrders(cookies)
     
